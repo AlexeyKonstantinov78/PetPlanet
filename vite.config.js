e246cf29c1f2a2dae8f14cc46afdeadc9729bde9
Aleksey Konstantinov Ubunty <alex_kav@mail.ru>
2024-04-19 15:18:19 +0300

    optimize image

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,36 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
+
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 80,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    quality: 80,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    quality: 70,
+  },
+  cache: false,
+  cacheLocation: undefined,
+};
 
 export default defineConfig({
   root: './src',
@@ -6,4 +38,9 @@ export default defineConfig({
   build: {
     outDir: '../dist',
   },
+  plugins: [
+    ViteImageOptimizer(
+      DEFAULT_OPTIONS
+    ),
+  ],
 });
