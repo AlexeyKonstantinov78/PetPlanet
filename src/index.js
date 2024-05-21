c9de1a938b96dab65b299af91fcfadb3231d7bb1
AlexeyKonstantinov78 <alex_kav@mail.ru>
2024-05-21 10:29:50 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const buttons = document.querySelectorAll('.store__categorry-button');
+const buttons = document.querySelectorAll('.store__category-button');
 
 const changeActiveBtn = (event) => {
   const target = event.target;
 
   buttons.forEach((button) => {
-    button.classList.remove('store__categorry-button_active');
+    button.classList.remove('store__category-button_active');
   });
 
-  target.classList.add('store__categorry-button_active');
+  target.classList.add('store__category-button_active');
 };
 
 buttons.forEach((button) => {
